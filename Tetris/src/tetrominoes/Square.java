8b34be3257676a2deecc068df7e05addb07ffe52
Liam Kreiss <liam.kreiss@gmail.com>
2016-02-22 11:34:49 -0800

    minor spacing

--- Tetris/src/tetrominoes/Square.java
+++ Tetris/src/tetrominoes/Square.java
@@ -3,10 +3,10 @@ package tetrominoes;
 public class Square extends Block {
 
 	public static final boolean[][] SHAPE = new boolean[][]{
-		{false, false, false ,false},
-		{false,true,true,false},
-		{false,true,true,false},
-		{false,false,false,false}
+		{false, false, 	false,	false},
+		{false,	true,	true,	false},
+		{false,	true,	true,	false},
+		{false,	false,	false,	false}
 	};
 	
 	public Square() {
