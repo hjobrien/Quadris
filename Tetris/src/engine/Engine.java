59a51d0bc48c9f217fd43db15043846cb230895d
Hank <hank.obrien900@icloud.com>
2016-02-22 11:45:18 -0800

    added engine

--- Tetris/src/engine/Engine.java
+++ Tetris/src/engine/Engine.java
@@ -4,10 +4,24 @@ import javafx.animation.AnimationTimer;
 
 public class Engine extends AnimationTimer {
 
+	public Engine(boolean[][] boardState){
+		
+	}
+	
+	
 	@Override
 	public void handle(long arg0) {
-		// TODO Auto-generated method stub
+		//makes the update only happen every second
+		//the update rate should increase as a function of score, but thats a (much) later feature
+		if((arg0 % 1e9) == 0){
+			update();
+		}
+	}
 
+	//do collision detection here I think
+	private void update() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
