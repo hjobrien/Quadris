7e37326bfbeaa561d33e8e0f4af6430138aa8a10
liamkreiss <liamkreiss@172.30.72.39>
2016-05-23 09:42:41 -0700

    --

--- Tetris/src/clients/DualPlayClient.java
+++ Tetris/src/clients/DualPlayClient.java
@@ -8,14 +8,14 @@ public class DualPlayClient extends Application {
 
 
 
-  private static final int GAME_HEIGHT = 20;
-  private static final int GAME_WIDTH = 10;
-  private static final int MIN_TIME_PER_TURN = 100000000;
-  private static final boolean USE_GRAPHICS = true;
-  private static final boolean DO_DEBUG = false;
-  private static final boolean RANDOMIZE = false;
-  private static final boolean PLAY_MULTIPLE = false;
-  private static final double[] WEIGHTS = new double[] {-70, -97.85, 306.77, 5};
+//  private static final int GAME_HEIGHT = 20;
+//  private static final int GAME_WIDTH = 10;
+//  private static final int MIN_TIME_PER_TURN = 100000000;
+//  private static final boolean USE_GRAPHICS = true;
+//  private static final boolean DO_DEBUG = false;
+//  private static final boolean RANDOMIZE = false;
+//  private static final boolean PLAY_MULTIPLE = false;
+//  private static final double[] WEIGHTS = new double[] {-70, -97.85, 306.77, 5};
 
   public static void main(String[] args) {
     launch();
