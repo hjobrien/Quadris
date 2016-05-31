1a17683d62a28d6480e3cb907cf50d8ed8749a71
Hank O'Brien <hank.obrien900@icloud.com>
2016-05-31 09:05:38 -0700

    Added blocks to the Blocks To Add file

--- Tetris/src/clients/AutoplayClient.java
+++ Tetris/src/clients/AutoplayClient.java
@@ -28,7 +28,7 @@ public class AutoplayClient extends Application implements Viewable, Autoplayabl
   // public static final boolean PLAY_MULTIPLE = true;
   public static final ScoreMode SCORE_MODE = ScoreMode.SIMPLE;
   public static final BlockGenerator GENERATOR = new StandardizeBlocks(0);
-  // public static final BlockGenerator GENERATOR = new RandomizeBlocks();
+//   public static final BlockGenerator GENERATOR = new RandomizeBlocks();
   public static final int MAX_GAMES_TO_PLAY = 50;
 
   public static void main(String[] args) {
